949794bb3c38b127be0bc88a42e24dd8b02ec5ce
Marek Counts <countsmarek@gmail.com>
2023-03-13 23:24:12 -0400

    updated sqlite to create if not exist

--- server/src/database/connection_manager.rs
+++ server/src/database/connection_manager.rs
@@ -1,4 +1,6 @@
-use sqlx::sqlite::SqlitePool;
+use std::{path::Path, env};
+
+use sqlx::sqlite::{SqlitePool, SqliteConnectOptions};
 
 pub struct ConnectionManager {
     pub pool: SqlitePool,
@@ -6,7 +8,29 @@ pub struct ConnectionManager {
 
 impl ConnectionManager {
     pub async fn new(url: String) -> anyhow::Result<Self> {
-        let pool = SqlitePool::connect(&url).await?;
+        // will create the db if needed
+        let url = SqliteConnectOptions::new()
+            .filename(url)
+            .create_if_missing(true);
+        let pool = SqlitePool::connect_with(url).await?;
+
+        // Run migrations
+        let migrations = if env::var("RUST_ENV") == Ok("production".to_string()) {
+            // Productions migrations dir
+            std::env::current_exe()?.join("./migrations")
+        } else {
+            // Development migrations dir
+            let crate_dir = std::env::var("CARGO_MANIFEST_DIR")?;
+            Path::new(&crate_dir)
+                .join("./migrations")
+        };
+    
+        sqlx::migrate::Migrator::new(migrations)
+            .await?
+            .run(&pool)
+            .await?;
+
+        // Return the connection manager
         Ok(Self { pool })
     }
 }
